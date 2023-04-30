7a9f72fa0d7773be106b965aa645149823cd3219
garydd1 <dgarizado@gmail.com>
2023-04-30 17:07:56 +0200

    updated lexic

--- minishell/lexic.c
+++ minishell/lexic.c
@@ -1,5 +1,48 @@
 #include "minishell.h"
 
+static int	ft_shift(char *str, int *index)
+{
+	int i;
+
+	i = *index;
+	while (str[i] != '\0')
+	{
+		str[i] = str[i  + 1];
+		i++;
+	}
+	return (0);
+}
+static int	ft_remove(char *str, int *index, char c)
+{
+	ft_shift(str, index);
+	while (str[*index] != '\0')
+	{
+		if (str[*index] == c)
+		{
+			ft_shift(str, index);
+			break ;
+		}
+		(*index)++;
+	}
+	return (0);
+}
+
+int ft_unquote(char *input)
+{
+	int	i;
+
+	i = 0;
+	while(input[i] != '\0')
+	{
+		if (input[i] == '\'' || input[i] == '\"' )
+		{
+			ft_remove(input, &i, input[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+
 static int	ft_is_closed(char *str, int *index, char c)
 {
 	(*index)++;
@@ -17,7 +60,6 @@ int	ft_lexic(char *input)
 	int	i;
 
 	i = 0;
-	
 	while(input[i] != '\0')
 	{
 		if (input[i] == '\'' || input[i] == '\"' )
@@ -27,5 +69,7 @@ int	ft_lexic(char *input)
 		}
 		i++;
 	}
+	ft_unquote(input);
+	printf("\nmodified: %s\n", input);
 	return (0);
 }
